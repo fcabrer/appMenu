7af35e75f30439734d67692d066f53b9c5cb9fd9
fcabrer <fcabrera.dnrpa@gmail.com>
2023-06-29 22:50:03 -0400

    Screen creados y barril actualizado

--- lib/presentation/home/screens.dart
+++ lib/presentation/home/screens.dart
@@ -1,3 +1,13 @@
+export 'package:flutter/material.dart';
+export 'package:widgets_app/presentation/animated/animated_screen.dart';
+export 'package:widgets_app/presentation/animated/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets_app/presentation/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/cards/cards_screen.dart';
 export 'package:widgets_app/presentation/home/home_screen.dart';
+export 'package:widgets_app/presentation/infinite_Scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/progress/progress_screen.dart';
+export 'package:widgets_app/presentation/ui_controls/ui_controls_screen.dart';
+
+
+
+//sort lines Ascending para ordenar las exportaciones
