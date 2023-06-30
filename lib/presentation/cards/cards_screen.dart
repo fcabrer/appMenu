fac49ff0b6ac64285309a0a15880c19ec4468782
fcabrer <fcabrera.dnrpa@gmail.com>
2023-06-29 22:21:24 -0400

    cards finish

--- lib/presentation/cards/cards_screen.dart
+++ lib/presentation/cards/cards_screen.dart
@@ -37,7 +37,15 @@ class _CardsView extends StatelessWidget {
           ...cards.map((card) =>
               _CardType1(label: card['label'], elevation: card['elevation'])),
           ...cards.map((card) =>
-              _CardType1(label: card['label'], elevation: card['elevation'])),
+              _CardType2(label: card['label'], elevation: card['elevation'])),
+          ...cards.map((card) =>
+              _CardType3(label: card['label'], elevation: card['elevation'])),
+          ...cards.map((card) =>
+              _CardType4(label: card['label'], elevation: card['elevation'])),
+
+          const SizedBox(
+              height:
+                  50), //cuando termine el scroll quede un espacio al finalizar
         ],
       ),
     );
@@ -71,6 +79,116 @@ class _CardType1 extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          side: BorderSide(color: colors.outline)),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType3({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      color: colors.surfaceVariant,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - Filled'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType4({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.hardEdge, //evita que el contenido se salga de su padre
+      elevation: elevation,
+      child: Stack(
+        //el stack lo que hace es ordenar los widget uno arriba del otro
+        children: [
+          Image.network(
+            'http://picsum.photos/id/${elevation.toInt()}/600/350',
+            height: 350, //tamaño de la img
+            fit: BoxFit.cover, //fit como queremos q se adapt la img al espacio
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(20))),
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
