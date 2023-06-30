918a73065fa0aacb00f2a22c6cac8cd714776dd1
fcabrer <fcabrera.dnrpa@gmail.com>
2023-06-30 00:25:54 -0400

    progress indicator finish

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -24,4 +24,9 @@ const appMenuItem = <MenuItem>[
       subTitle: 'Un contenedor estilizado',
       link: '/cards',
       icon: Icons.credit_card),
+  MenuItem(
+      title: 'ProgressIndicators',
+      subTitle: 'Generales y controlados',
+      link: '/progress',
+      icon: Icons.refresh_rounded),
 ];
--- lib/presentation/home/screens.dart
+++ lib/presentation/home/screens.dart
@@ -1,3 +1,4 @@
+export 'package:widgets_app/presentation/progress/progress_screen.dart';
 export 'package:flutter/material.dart';
 export 'package:widgets_app/presentation/animated/animated_screen.dart';
 export 'package:widgets_app/presentation/animated/app_tutorial/app_tutorial_screen.dart';
@@ -5,7 +6,7 @@ export 'package:widgets_app/presentation/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/cards/cards_screen.dart';
 export 'package:widgets_app/presentation/home/home_screen.dart';
 export 'package:widgets_app/presentation/infinite_Scroll/infinite_scroll_screen.dart';
-export 'package:widgets_app/presentation/progress/progress_screen.dart';
+
 export 'package:widgets_app/presentation/ui_controls/ui_controls_screen.dart';
 
 
