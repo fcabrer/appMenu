5fc2480fccdd00a7e622b67dc17a177478bbeab6
fcabrer <fcabrera.dnrpa@gmail.com>
2023-07-03 23:22:02 -0400

    ui controls terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -34,4 +34,14 @@ const appMenuItem = <MenuItem>[
       subTitle: 'Indicadores de pantalla',
       link: '/snackbarscreen',
       icon: Icons.info_outline),
+  MenuItem(
+      title: 'Animated container',
+      subTitle: 'Stateful widget animado',
+      link: '/animated',
+      icon: Icons.check_box_outline_blank_rounded),
+  MenuItem(
+      title: 'UI Controls + Tiles',
+      subTitle: 'Una serie de controles',
+      link: '/ui-controls',
+      icon: Icons.car_rental_outlined),
 ];
